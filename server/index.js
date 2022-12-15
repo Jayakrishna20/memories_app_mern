79b3c724b42d5a9c5f6a9868a73279e67726e295
Jayakrishna20 <jayakrishnan2041@gmail.com>
2022-12-15 21:52:58 +0530

    redux function implemented

--- server/index.js
+++ server/index.js
@@ -3,3 +3,27 @@ import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
 import cors from 'cors';
 
+import postRoutes from './routes/posts.js';
+
+const app = express();
+
+app.use(bodyParser.json({ limit: '30mb', extended: true }));
+app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }));
+app.use(cors());
+
+app.use('/posts', postRoutes);
+
+const CONNECTION_URL =
+  'mongodb+srv://jayakrishnan:2rqEp-vR-RZNuvE@cluster0.eojpfdh.mongodb.net/?retryWrites=true&w=majority';
+const PORT = process.env.PORT || 5000;
+
+mongoose
+  .connect(CONNECTION_URL, {
+    useUnifiedTopology: true,
+  })
+  .then(() =>
+    app.listen(PORT, () => console.log(`Server running on port: ${PORT}`))
+  )
+  .catch((error) => console.log(error.message));
+
+mongoose.set('strictQuery', true);
